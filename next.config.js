6a797a9ccea1c325cfa219c2d4d7412344020b59
Dabbcomputers <dabbcomputers@gmail.com>
2025-03-15 10:08:39 -0700

    Form fix try 6

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    output: 'export', // Static HTML export
-    // Other config options you might need
-  };
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [{ key: "Content-Type", value: "text/html" }],
+      },
+    ];
+  },
+  output: 'export', // Static HTML export
+  // Other config options you might need
+};
   
